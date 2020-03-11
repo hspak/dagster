d1538c85cd03ae76e1e3965b5d95371b185d8539
catherinewu <catherine.cw.wu@gmail.com>
2020-03-11 00:57:21 -0700

    Initial pass at telemetry
    Summary: Initial pass at telemetry. In progress: update docs + standardize verbiage
    
    Test Plan: Added some tests
    
    Differential Revision: https://dagster.phacility.com/D2233

--- python_modules/dagster/dagster/cli/utils.py
+++ python_modules/dagster/dagster/cli/utils.py
@@ -7,11 +7,19 @@ import six
 
 from dagster import check
 from dagster.core.execution.compute_logs import tail_polling
+from dagster.core.telemetry import (
+    execute_disable_telemetry,
+    execute_enable_telemetry,
+    execute_reset_telemetry_profile,
+)
 
 
 def create_utils_cli_group():
     group = click.Group(name="utils")
     group.add_command(tail_command)
+    group.add_command(reset_telemetry_profile)
+    group.add_command(disable_telemetry)
+    group.add_command(enable_telemetry)
     return group
 
 
@@ -38,6 +46,36 @@ def tail_command(filepath, parent_pid, io_type):
     io_type = extract_string_param('io_type', io_type)
     stream = sys.stdout if io_type == 'stdout' else sys.stderr
     tail_polling(filepath, stream, parent_pid)
+
+
+@click.command(
+    name='reset_telemetry_profile',
+    help='Generate a new user identifier for telemetry. Requires $DAGSTER_HOME to be set. \
+        For more information, check out the docs at \
+        https://docs.dagster.io/latest/install/telemetry/',
+)
+def reset_telemetry_profile():
+    execute_reset_telemetry_profile()
+
+
+@click.command(
+    name='disable_telemetry',
+    help='Update $DAGSTER_HOME/dagster.yaml to disable telemetry. Requires $DAGSTER_HOME to be set \
+        For more information, check out the docs at \
+        https://docs.dagster.io/latest/install/telemetry/',
+)
+def disable_telemetry():
+    execute_disable_telemetry()
+
+
+@click.command(
+    name='enable_telemetry',
+    help='Update $DAGSTER_HOME/dagster.yaml to enable telemetry. Requires $DAGSTER_HOME to be set \
+        For more information, check out the docs at \
+        https://docs.dagster.io/latest/install/telemetry/',
+)
+def enable_telemetry():
+    execute_enable_telemetry()
 
 
 utils_cli = create_utils_cli_group()
